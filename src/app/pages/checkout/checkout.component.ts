774418986c8806ec559a418401c8dcd2ff4c8291
Daniel Pincay <alexispincay005@gmail.com>
2022-02-01 20:39:04 -0500

    empezando guardar orden de compra

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { Store } from 'src/app/shared/interface/store.interface';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,18 +15,24 @@ export class CheckoutComponent implements OnInit {
     shippingAddress:'',
     city:''
   }
-  tiendas=[{
-    "id": 2,
-    "name": "Store Alcalá",
-    "address": "Calle de Alcalá, 21",
-    "city": "Madrid",
-    "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-}]
-  constructor() { }
+  needDelivery:boolean=false
+  tiendas:Store[]=[]
+  constructor(private dataSvc:DataService) { }
 
   ngOnInit(): void {
+    this.getStores()
   }
   onPickUp(value:boolean){
-    console.log(value)
+    this.needDelivery=value
+  }
+  onSubmit(){
+    console.log("ey")
+  }
+  getStores():void{
+    this.dataSvc.getStores().pipe(
+      tap((stores:Store[])=>{
+       this.tiendas=stores
+      })
+    ).subscribe()
   }
 }
---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Store } from "../interface/store.interface";
+
+
+@Injectable({
+    providedIn:'root'
+})
+
+
+export class DataService {
+    private apiUrl='http://localhost:3000';
+
+    constructor(private http:HttpClient){
+
+    }
+    getStores():Observable<Store[]>{
+        return this.http.get<Store[]>(`${this.apiUrl}/stores`)
+    }
+}
